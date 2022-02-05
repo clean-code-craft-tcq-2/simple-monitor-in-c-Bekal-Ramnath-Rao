c998cf3557fbee5a8616d7ca64281ad55dd7a0a9
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-02-05 15:32:32 +0530

    reduced code lines in compareBatteryParameter

--- checker.c
+++ checker.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <assert.h>
 
+// Defining the thresholds as global variable, why? --> if different customer need  different threshold
+#define TEMP_LIMIT_LOW 0
+#define TEMP_LIMIT_HIGH 45
+
+#define SOC_LIMIT_LOW 20
+#define SOC_LIMIT_HIGH 80
+
+#define CHARGERATE_LIMIT_LOW 0
+#define CHARGERATE_LIMIT_HIGH 0.8
+
 int compareBatteryParameter(float value,float lower_limit,float upper_limit)
 {
-	if( value<lower_limit || value>upper_limit)
-	{
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
+	return (!(value<lower_limit || value>upper_limit));
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
-	return (compareBatteryParameter(temperature,0,45)&&
-		    compareBatteryParameter(soc,20,80)&&
-		    compareBatteryParameter(chargeRate,0,0.8));
+	return (compareBatteryParameter(temperature,TEMP_LIMIT_LOW,TEMP_LIMIT_HIGH)&&
+		    compareBatteryParameter(soc,SOC_LIMIT_LOW,SOC_LIMIT_HIGH)&&
+		    compareBatteryParameter(chargeRate,CHARGERATE_LIMIT_LOW,CHARGERATE_LIMIT_HIGH));
 
 }
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
+  assert(!batteryIsOk(50,50,0.5));
+  assert(!batteryIsOk(40,85,0.5));
+  assert(!batteryIsOk(40,85,1.0));
 }
